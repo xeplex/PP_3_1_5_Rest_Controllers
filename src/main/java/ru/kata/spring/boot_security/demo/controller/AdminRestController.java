1e9be5317abf8118ebfa997a736bc853b1037260
xeplex <umnikov1337@gmail.com>
2025-01-13 13:48:01 +0300

    отправляю на проверку

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminRestController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.exception_handler.NoSuchUserException;
 import ru.kata.spring.boot_security.demo.model.User;
-import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.UserService;
 import ru.kata.spring.boot_security.demo.validation.ValidateRestUserService;
 
